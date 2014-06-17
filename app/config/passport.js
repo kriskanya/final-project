59c470a41518829c5867b092b7160754b4da992a
Kristian Kanya <kriskanya@gmail.com>
2014-06-16 22:30:25 -0500

    need to fix local login

--- app/config/passport.js
+++ app/config/passport.js
@@ -142,18 +142,22 @@ module.exports = function(passport) {
 		// pull in our app id and secret from our auth.js file
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
-        callbackURL     : configAuth.facebookAuth.callbackURL
+        callbackURL     : configAuth.facebookAuth.callbackURL,
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
 
     },
 
     // facebook will send back the token and profile
-    function(token, refreshToken, profile, done) {
+    function(req, token, refreshToken, profile, done) {
 
 		// asynchronous
 		process.nextTick(function() {
 
-			// find the user in the database based on their facebook id
-	        userCollection.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+      // check if the user is already logged in
+        	if (!req.user) {
+
+  			// find the user in the database based on their facebook id
+  	        userCollection.findOne({ 'facebook.id' : profile.id }, function(err, user) {
 
 	        	// if there is an error, stop everything and return that
 	        	// ie an error connecting to the database
@@ -162,7 +166,22 @@ module.exports = function(passport) {
               }
 
 				// if the user is found, then log them in
-	            if (user) {
+	            if (user){
+                // if there is a user id already but no token (user was linked at one point and then removed)
+                // just add our token and profile information
+                if (!user.facebook.token) {
+                    user.facebook.token = token;
+                    user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    user.facebook.email = profile.emails[0].value;
+
+                    user.save(function(err) {
+                        if (err){
+                          throw err;
+                        }
+                        return done(null, user);
+                    });
+                  }
+
 	                return done(null, user); // user found, return that user
 	            } else {
 	                // if there is no user found with that facebook id, create them
@@ -183,10 +202,28 @@ module.exports = function(passport) {
 	                    // if successful, return the new user
 	                    return done(null, newUser);
 	                });
-	            }
+  	            }
 
-	        });
-        });
+  	        });
+        }else{
+          // user already exists and is logged in, we have to link accounts
+	            var user            = req.user; // pull the user out of the session
+
+				// update the current users facebook credentials
+	            user.facebook.id    = profile.id;
+	            user.facebook.token = token;
+	            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+	            user.facebook.email = profile.emails[0].value;
+
+				// save the user
+	            user.save(function(err) {
+	                if (err){
+                    throw err;
+                  }
+	                return done(null, user);
+          });
+        }
+      });
 
     }));
 
@@ -219,6 +256,22 @@ module.exports = function(passport) {
 
 				// if the user is found then log them in
 	            if (user) {
+
+          // if there is a user id already but no token (user was linked at one point and then removed)
+            	// just add our token and profile information
+                if (!user.twitter.token) {
+                    user.twitter.token = token;
+                    user.twitter.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    user.twitter.email = profile.emails[0].value;
+
+                    user.save(function(err) {
+                        if (err){
+                          throw err;
+                        }
+                        return done(null, user);
+                    });
+                  }
+
 	                return done(null, user); // user found, return that user
 	            } else {
 	                // if there is no user, create them
--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -126,6 +126,77 @@ function load(app, fn){
 
 
 
+  // =============================================================================
+// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+// =============================================================================
+
+	// locally --------------------------------
+		app.get('/connect/local', function(req, res) {
+			res.render('users/connect-local', { message: req.flash('loginMessage') });
+		});
+		app.post('/connect/local', passport.authenticate('local-signup', {
+			successRedirect : '/profile', // redirect to the secure profile section
+			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+			failureFlash : true // allow flash messages
+		}));
+
+	// facebook -------------------------------
+
+		// send to facebook to do the authentication
+		app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+		// handle the callback after facebook has authorized the user
+		app.get('/connect/facebook/callback',
+			passport.authorize('facebook', {
+				successRedirect : '/profile',
+				failureRedirect : '/'
+			}));
+
+	// twitter --------------------------------
+
+		// send to twitter to do the authentication
+		app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+
+		// handle the callback after twitter has authorized the user
+		app.get('/connect/twitter/callback',
+			passport.authorize('twitter', {
+				successRedirect : '/profile',
+				failureRedirect : '/'
+			}));
+
+
+    //unlink routes
+
+    // local -----------------------------------
+    app.get('/unlink/local', function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+    // facebook -------------------------------
+    app.get('/unlink/facebook', function(req, res) {
+        var user            = req.user;
+        user.facebook.token = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+    // twitter --------------------------------
+    app.get('/unlink/twitter', function(req, res) {
+        var user           = req.user;
+        user.twitter.token = undefined;
+        user.save(function(err) {
+           res.redirect('/profile');
+        });
+    });
+
+    //end unlink routes
+
   console.log('Routes Loaded');
   fn();
 }
